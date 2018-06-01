3eabe8e5803ed7562ae93a073ff68d6a838141bc
Yessenia Hernandez Hernandez <yh90926@srcs.loc>
2018-06-01 12:08:04 -0700

    finished

--- rochambeau.js
+++ rochambeau.js
@@ -1,16 +1,12 @@
-// Stores the current player's and computer's choices
-// 0 = Rock, 1 = Paper, 2 = Scissors, 3 = Lizard, 4 = Spock
 var playerChoice;
 var computerChoice;
 
-// Stores the lables for the choices
 var choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
 
 // Variable to store the score
 // score[0] = wins, score[1] = ties, score[2] = losses
-var score = [0,0,0,0,0];
-// matches[0] = my wins, score [1] =  computerwins
-var matches = [0,0]
+var score = [0, 0, 0];
+var matches = [0, 0];
 
 // Stores the player's choice, then call's the function for storing the computer's choice
 function storePlayerChoice(choice) {
@@ -21,52 +17,49 @@ function storePlayerChoice(choice) {
 
 // Generate computer's random choice
 function storeComputerChoice() {
-    computerChoice = Math.floor(Math.random()*3);
+    computerChoice = Math.floor(Math.random() * 5);
     console.log("Computer choice = " + computerChoice);
 }
 
 // This is the function for playing the game
-function playGame(){
-    // Here is the game ruleset algorithm
+function playGame() {
     if (playerChoice == computerChoice) {
-        // We have a tie!
         updateScore(1);
-        displayGameResult("tie")
+        displayGameResult("tie");
     } else if (playerChoice == 0 && (computerChoice == 2 || computerChoice == 4)) {
-        // Rock beats scissors - a win!
         updateScore(0);
-        updateMacth(1);
-        displayGameResult("win")
+        displayGameResult("win");
     } else if (playerChoice == 1 && (computerChoice == 0 || computerChoice == 3)) {
-        // Paper beats scissors - a win!
         updateScore(0);
-        updateMacth(1);
-        displayGameResult("win")
+        displayGameResult("win");
     } else if (playerChoice == 2 && (computerChoice == 1 || computerChoice == 4)) {
-        // Scissors beats paper - a win!
         updateScore(0);
-        updateMacth(1);
-        displayGameResult("win")
-         } else if (playerChoice == 3 && (computerChoice == 2 || computerChoice == 4)) {
-        // Rock beats scissors - a win!
+        displayGameResult("win");
+    } else if (playerChoice == 3 && (computerChoice == 2 || computerChoice == 4)) {
         updateScore(0);
-             updateMacth(1);
-        displayGameResult("win")
-         } else if (playerChoice == 4 && (computerChoice == 3 || computerChoice == 0)) {
-        // Rock beats scissors - a win!
+        displayGameResult("win");
+    } else if (playerChoice == 4 && (computerChoice == 3 || computerChoice == 0)) {
         updateScore(0);
-             updateMacthes(1);
-        displayGameResult("win")
+        displayGameResult("win");
     } else {
-        // All other combinations are losses
         updateScore(2);
-        displayGameResult("lose")
-        updateMacth(0);
+        displayGameResult("lose");
+
     }
+
+
+
+if (score[0] == 2) {
+    updateMatch(0);
+    score = [0, 0, 0];
+} else if (score[2] == 2) {
+    updateMatch(1);
+    score = [0, 0, 0];
 }
 
+}
 //Displays the result of the game
-function displayGameResult(result){
+function displayGameResult(result) {
     // Define an array of text labels for the choices 0, 1, 2;
     // Create a message for the player
     var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ".";
@@ -86,33 +79,33 @@ function displayGameResult(result){
     }
 
     updateScoreBoard();
-    updateMatchScoreBoard();
 }
 
 // Updates the score
-function updateScore(val){
+function updateScore(val) {
     ++score[val];
     console.log("The score is now " + score);
 }
 
-function updateMatch(val){
-    ++matches[val];
-    console.log("The Match score is now " + matches);
+
+function updateMatch(a) {
+    ++matches[a];
+    console.log("The Match score is now " + matches)
 }
 
 
+
+
 // Function for displaying the score
-function updateScoreBoard(){
+function updateScoreBoard() {
     document.getElementById("wins").textContent = score[0];
     document.getElementById("losses").textContent = score[2];
     document.getElementById("ties").textContent = score[1];
+
+    document.getElementById("matchwins").textContent = matches[0];
+    document.getElementById("matchlosses").textContent = matches[1];
 }
 
-// Function for displaying the score
-function updateMatchScoreBoard(){
-    document.getElementById("myMatcheswon").textContent = matches[0];
-    document.getElementById("computerMatcheswon").textContent = matches[1];
-}
 
 // The button elements
 var rockButton = document.getElementById("rock");
@@ -123,9 +116,21 @@ var spockButton = document.getElementById("spock");
 var playButton = document.getElementById("play");
 
 // Add the event handlers
-rockButton.addEventListener('click', () => {storePlayerChoice(0)});
-paperButton.addEventListener('click', () => {storePlayerChoice(1)});
-scissorsButton.addEventListener('click', () => {storePlayerChoice(2)});
-lizardButton.addEventListener('click', () => {storePlayerChoice(3)});
-spockButton.addEventListener('click', () => {storePlayerChoice(4)});
-playButton.addEventListener('click', () => {playGame()});
+rockButton.addEventListener('click', () => {
+    storePlayerChoice(0)
+});
+paperButton.addEventListener('click', () => {
+    storePlayerChoice(1)
+});
+scissorsButton.addEventListener('click', () => {
+    storePlayerChoice(2)
+});
+lizardButton.addEventListener('click', () => {
+    storePlayerChoice(3)
+});
+spockButton.addEventListener('click', () => {
+    storePlayerChoice(4)
+});
+playButton.addEventListener('click', () => {
+    playGame()
+});
